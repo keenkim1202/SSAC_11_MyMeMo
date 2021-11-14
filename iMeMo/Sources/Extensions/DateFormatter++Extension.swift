dd88a76cb41a6d7ee77628ae6cf5aeabef273a4c
KEEN <homurahomu@naver.com>
2021-11-15 01:08:30 +0900

    기간 별 dateFormatter extension에 작성

--- iMeMo/Sources/Extensions/DateFormatter++Extension.swift
+++ iMeMo/Sources/Extensions/DateFormatter++Extension.swift
@@ -8,9 +8,24 @@
 import Foundation
 
 extension DateFormatter {
-  static var customFormat: DateFormatter {
+  static var etcFormat: DateFormatter { // 그 외의 기간
+    let df = DateFormatter()
+    df.locale = Locale(identifier: "Ko_kr")
+    df.dateFormat = "yyyy.MM.dd a hh:mm"
+    return df
+  }
+  
+  static var weekdayFormat: DateFormatter { // 이번주
     let date = DateFormatter()
-    date.dateFormat = "yyyy.MM.dd a hh:mm"
+    date.locale = Locale(identifier: "Ko_kr")
+    date.dateFormat = "EEEE"
+    return date
+  }
+  
+  static var todayFormat: DateFormatter { // 오늘
+    let date = DateFormatter()
+    date.locale = Locale(identifier: "Ko_kr")
+    date.dateFormat = "a hh:mm"
     return date
   }
 }
