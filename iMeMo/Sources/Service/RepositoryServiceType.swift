e0c71840e4abe343849ea51af2cf52da3353cab2
KEEN <homurahomu@naver.com>
2021-11-14 15:10:15 +0900

    search 시, filter 조건 변경

--- iMeMo/Sources/Service/RepositoryServiceType.swift
+++ iMeMo/Sources/Service/RepositoryServiceType.swift
@@ -79,6 +79,7 @@ final class RepositoryService: RepositoryServiceType {
   
   func search(query: String) -> Results<Memo> {
     let search = localRealm.objects(Memo.self)
+      .filter("isEnable == true")
       .filter("title CONTAINS[c] '\(query)' OR content CONTAINS[c] '\(query)'")
     return search.sorted(byKeyPath: "writtenDate", ascending: false)
   }
